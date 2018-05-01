fd998b8b48bbdb9ed954187b10dc91b700b04552
Rockwellwindsor <rockwellwindsor@gmail.com>
2018-05-01 18:16:03 -0500

    Development: Began Issue Tracker Application

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,43 @@
-var contentNode = document.getElementById('contents');
-const component = <h1>Hello World!</h1>;
-ReactDOM.render(component, contentNode);
+const contentNode = document.getElementById('contents');
+
+class IssueAdd extends React.Component {
+  render() {
+    return (
+      <div>This is a placeholder for the issue add form.</div>
+    )
+  }
+}
+
+class IssueFilter extends React.Component {
+  render() {
+    return (
+      <div>This is a placeholder for the issue filter.</div>
+    )
+  }
+}
+
+class IssueTable extends React.Component {
+  render() {
+    return (
+      <div>This is a placeholder for the issue table.</div>
+    )
+  }
+}
+
+class IssueList extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Issue Tracker</h1>
+        <hr />
+        <IssueFilter />
+        <hr />
+        <IssueTable />
+        <hr />
+        <IssueAdd />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<IssueList />, contentNode);
